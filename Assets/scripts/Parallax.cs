cf377c72371b1f4f7606bc8402ac4287b75b2f47
SpookyRobin <robinonderstal30@gmail.com>
2022-05-11 14:30:06 +0200

    parallax

--- Assets/scripts/Parallax.cs
+++ Assets/scripts/Parallax.cs
@@ -6,17 +6,18 @@ public class Parallax : MonoBehaviour
 {
     private float length, startpos;
     public GameObject cam;
-    public float parallaxEffect;
-
-    // Start is called before the first frame update
+    public float parallexEffect;
     void Start()
     {
-        
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
