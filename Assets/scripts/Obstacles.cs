5e73b0059d77f963ae2ccc2957f173620401263a
SpookyRobin <robinonderstal30@gmail.com>
2022-05-16 14:58:45 +0200

    birb

---
+++ Assets/scripts/Obstacles.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Obstacle", menuName ="Obstacle")]
+public class Obstacles : ScriptableObject
+{
+    public Sprite sprite;
+    public float velocity;
+}
